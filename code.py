507e1f24fca2cbce3bf60d83dc3e7a1921247ee0
Keenan Robinson <rbnkee001@myuct.ac.za>
2019-07-26 15:24:35 +0100

    Copied the template from mypracs. code.py has LED with delay, code1.py has code using an interrupt for button press.

--- code.py
+++ code.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
 """
-Python Practical Template
-Keegan Crankshaw
-Readjust this Docstring as follows:
-Names: <names>
-Student Number: <studnum>
-Prac: <Prac Num>
-Date: <dd/mm/yyyy>
+Names: Keenan Robinson
+Student Number: RBNKEE001
+Prac: 1
+Date: 26/07/2019
 """
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+import time
+#initialisations
+GPIO.setmode(GPIO.BOARD)
+LED1 = 11
+GPIO.setup(LED1,GPIO.OUT, initial = GPIO.HIGH)		
+#Pin 17 on raspberry connected to LED 1		
+#initial state must be LOW
 
 # Logic that you write
 def main():
-    print("write your logic here")
+    GPIO.output(LED1,GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(LED1,GPIO.HIGH)
+    time.sleep(1)
+    
 
 
 # Only run the functions if 
@@ -27,7 +35,7 @@ if __name__ == "__main__":
         print("Exiting gracefully")
         # Turn off your GPIOs here
         GPIO.cleanup()
-    except e:
+    except Exception as e:
         GPIO.cleanup()
         print("Some other error occurred")
         print(e.message)
---
+++ code1.py
@@ -0,0 +1,64 @@
+"""
+Names: Keenan Robinson
+Student Number: RBNKEE001
+Prac: 1
+Date: 26/07/2019
+"""
+# Program designed to toggle an LED off and on
+# import Relevant Librares
+import RPi.GPIO as GPIO
+import time
+#initialisations
+GPIO.setmode(GPIO.BOARD)
+#Pin numbers
+LED1 = 11
+BUTTON1=13
+#Pin setups
+GPIO.setup(LED1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(BUTTON1, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+#Variable initialisations
+high_pin = False
+
+def button_detect(channel):
+    print('Falling Edge detected!')
+    global high_pin
+    if high_pin==False:
+    	GPIO.output(LED1, GPIO.HIGH)
+	high_pin = True
+        print('LED ON')
+    else:
+    	GPIO.output(LED1, GPIO.LOW)
+	high_pin = False
+	print('LED OFF')
+#Interrupt for when the button goes to a low value
+GPIO.add_event_detect(BUTTON1, GPIO.FALLING, callback=button_detect, bouncetime = 150)
+
+
+# Logic that you write
+def main():
+    """ Code to turn the LED on and off with a 1 second delay
+    GPIO.output(LED1,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(LED1,GPIO.LOW)
+    time.sleep(1)
+    """
+    print("Waiting...")
+    time.sleep(1)
+ 
+# Only run the functions if 
+if __name__ == "__main__":
+    # Make sure the GPIO is stopped correctly
+    try:
+        while True:
+            main()
+    except KeyboardInterrupt:
+        print("Exiting gracefully")
+        # Turn off your GPIOs here
+        GPIO.cleanup()
+    except Exception as e:
+        GPIO.cleanup()
+        print("Some other error occurred")
+        print(e.message)
+
+
+
